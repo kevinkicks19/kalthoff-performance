3b201c3594ea40cc135332e9905f1c3ec9c2d11a
kevinkicks19 <kevinkicks19@gmail.com>
2025-04-05 00:32:30 -0600

    Major updates: Added testimonials page, fixed CNWST slideshow, updated About page images, added Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import sentry from "@sentry/astro";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    sentry({
+      dsn: "YOUR_DSN_HERE",
+      enabled: import.meta.env.PROD,
+      sourceMapsUploadOptions: {
+        project: "kalthoff-performance",
+        authToken: process.env.SENTRY_AUTH_TOKEN
+      }
+    })
+  ],
   site: 'https://kalthoffperformance.com',
   base: '',
   output: 'static'
